dda7a2bcc8c9c828e2bc836e6f3a9838be3c5fb8
asen15 <aniruddha.sen15@gmail.com>
2018-10-23 20:24:34 -0400

    provisioned users and groups from data bags

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,21 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+search("users", "gid:0").each do |user_data|
+	user user_data['id'] do
+		comment user_data['comment']
+		uid user_data['uid']
+		gid user_data['gid']
+		home user_data['home']
+		shell user_data['shell']
+	end
+end
+
+search("groups", "platform:centos").each do |group_data|
+	group group_data['id'] do
+		gid group_data['gid']
+		members group_data['members']
+	end
+end
+
